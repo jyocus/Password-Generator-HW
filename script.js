9233801f8e6de9899e2b146e23062d5097aa1295
Justin Yocus <justin.o.yocus@gmail.com>
2020-09-27 23:18:42 -0500

    stuck on a few parts

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
-// mincharCount = prompt("How many characters would you like?");
-// lowerCase = confirm("Would you like to include lowercase letters?");
-// upperCase = confirm("Would you like to include uppercase letters?");
-// includeNum = confirm("Would you like to include numbers?");
-// includeSpecial = confirm("Would you like to include special characters?"); 
+// var mincharCount = 8
+// var lowerCase = true
+// var upperCase = true
+// var includeNum = true
+// var includeSpecial = true
+
+
+// var mincharCount = prompt("How many characters would you like?");
+//   // if (value > 128 || value < 8)
+//   //   alert("Your password must be between 8 and 128 characters")
+//   // else (
+//     // (value > 128 || value < 8)
+//   //   alert("Your password must be between 8 and 128 characters")
+//   )
+
+// var lowerCase = confirm("Would you like to include lowercase letters?");
+// var upperCase = confirm("Would you like to include uppercase letters?");
+// var includeNum = confirm("Would you like to include numbers?");
+// var includeSpecial = confirm("Would you like to include special characters?"); 
 
 
 // How to generate the password - https://net-comber.com/charset.html for reference of unicode
@@ -23,6 +37,18 @@ function includeSpecial  () {
   var special = "!@#$%^&*(){}[]=<>/,.|~?";
   return special[Math.floor(Math.random()*Symbol.length)];
     }
+
+    console.log(includeSpecial.toString()); 
+//This will store this info from the above functions
+
+const randomGenerator = {
+  lower : lowerCase,
+  upper : upperCase,
+  number : includeNum
+ 
+
+};
+
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
  
@@ -36,7 +62,6 @@ function writePassword () {
 
 }
 
-console.log(password{i});
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
