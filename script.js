e745cb65c8e137f622c66734e2c4b095051da070
Justin Yocus <justin.o.yocus@gmail.com>
2020-11-06 20:24:34 -0600

    prompt/confirm flow established, validate number of characters

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
-function writePassword () {
+function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
@@ -11,15 +11,6 @@ function writePassword () {
 }
  
 
-// Write password to the #password input
-function writePassword () {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 //Variables for each character in the password
 var lowerCase = 'abcdefghijklmnopqrstuvwxyz';
 //https://www.w3schools.com/jsref/jsref_touppercase.asp#:~:text=The%20toUpperCase()%20method%20converts,a%20string%20to%20lowercase%20letters.
@@ -27,10 +18,23 @@ var upperCase = lowerCase.toUpperCase();
 var numbers = '1234567890'
 var specialChar = ' !#$%&()*+,-./:;<=>?@[^_`{|}~';
 
+function generatePassword (){
+
 var beginPrompt = prompt('How many characters do you want?');
+var passwordLength = parseInt(beginPrompt);
 
+//Checks the length of the password the user has chosen
+if (passwordLength >= 8 && passwordLength <=128) {
+  var chooseLowerCase = confirm('Do you want to use lowercase letters?');
+  var chooseUpperCase = confirm('Do you want to use uppercase letters?');
+  var chooseNumbers = confirm('Do you want to use numbers?');
+  var chooseSpecialChar = confirm('Do you want to use special characters?');
 
-
+} else {
+  alert('Your password must be between 8 and 128 characters')
+  generatePassword();
+}
+};
 
 // var mincharCount = prompt("How many characters would you like?");
 //   if (value > 128 || value < 8)
